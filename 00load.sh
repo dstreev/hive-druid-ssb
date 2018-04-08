ad07e5416c4c0df5baa00034f1cd06704af19099
David Streever <david@streever.com>
2018-04-08 09:31:49 -0400

    Manage against my cluster.

--- 00load.sh
+++ 00load.sh
@@ -2,9 +2,11 @@
 
 SCALE=${1:-2}
 HS2=${2:-localhost:10000}
-DRUID_HOST=${3:-$(hostname)}
-DRUID_USERNAME=${4:-druid}
-DRUID_PASSWORD=${5:-password}
+DRUID_META_HOST=${3:-$(hostname)}
+DRUID_META_USERNAME=${4:-druid}
+DRUID_META_PASSWORD=${5:-password}
+DRUID_BROKER_HOST=${5:-localhost}
+DRUID_COORD_HOST=${5:-localhost}
 BEELINE="beeline -u jdbc:hive2://$HS2/default"
 
-$BEELINE -f queries.druid/index_ssb.sql --hivevar DRUID_HOST=${DRUID_HOST} --hivevar DRUID_USERNAME=${DRUID_USERNAME} --hivevar DRUID_PASSWORD=${DRUID_PASSWORD} --hivevar SCALE=${SCALE} -n ${USER}
+$BEELINE -f queries.druid/index_ssb.sql --hivevar DRUID_COORD_HOST=${DRUID_COORD_HOST} --hivevar DRUID_BROKER_HOST=${DRUID_BROKER_HOST} --hivevar DRUID_META_HOST=${DRUID_META_HOST} --hivevar DRUID_USERNAME=${DRUID_META_USERNAME} --hivevar DRUID_PASSWORD=${DRUID_META_PASSWORD} --hivevar SCALE=${SCALE} -n ${USER}
--- 00run.sh
+++ 00run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-SCALE=${1:-2}
-TARGET=${2:-localhost:10000}
+TARGET=${1:-localhost:10000}
 BEELINE="beeline -u jdbc:hive2://${TARGET}/default"
 
 $BEELINE -f queries.druid/allqueries.sql
