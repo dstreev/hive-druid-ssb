4a8772f554562cfbe64496f2f44f7a78f6d60c7b
David Streever <david@streever.com>
2018-04-08 20:53:40 -0400

    bug

--- 00load.sh
+++ 00load.sh
@@ -9,7 +9,12 @@ DRUID_BROKER_HOST=${6:-localhost}
 DRUID_COORD_HOST=${7:-localhost}
 HIVE_USER=${8:-$USER}
 HIVE_PASSWORD_FILE=${9:-.hive_password}
-BEELINE="beeline -n $HIVE_USER -w $HIVE_PASSWORD_FILE -u jdbc:hive2://$HS2/default -f queries.druid/index_ssb.sql --hiveconf DRUID_COORD_HOST=${DRUID_COORD_HOST} --hiveconf DRUID_BROKER_HOST=${DRUID_BROKER_HOST} --hiveconf DRUID_META_HOST=${DRUID_META_HOST} --hiveconf DRUID_META_USERNAME=${DRUID_META_USERNAME} --hiveconf DRUID_META_PASSWORD=${DRUID_META_PASSWORD} --hiveconf SCALE=${SCALE}"
+BEELINE="beeline -n $HIVE_USER -w $HIVE_PASSWORD_FILE -u jdbc:hive2://$HS2/default -f queries.druid/index_ssb.sql --hiveconf SCALE=${SCALE}"
+# --hiveconf DRUID_COORD_HOST=${DRUID_COORD_HOST}
+# --hiveconf DRUID_BROKER_HOST=${DRUID_BROKER_HOST}
+# --hiveconf DRUID_META_HOST=${DRUID_META_HOST}
+# --hiveconf DRUID_META_USERNAME=${DRUID_META_USERNAME}
+# --hiveconf DRUID_META_PASSWORD=${DRUID_META_PASSWORD}
 
 echo $BEELINE
 
