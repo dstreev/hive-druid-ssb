f17434921e50a3d0096c01e79486bc4bcc24a70b
David Streever <david@streever.com>
2018-04-17 15:05:56 -0400

    params

--- ble.sh
+++ ble.sh
@@ -3,6 +3,7 @@
 . ./init-env.sh $@
 . ./check-env.sh
 
-BEELINE="beeline -n ${HIVE_USER} -w ${HIVE_PASSWORD_FILE} -u '${HS2}'"
+SOURCE=ssb_${SCALE}_flat_orc
+BEELINE="beeline -n ${HIVE_USER} -w ${HIVE_PASSWORD_FILE} -u '${HS2}' --hivevar SCALE=${SCALE} --hivevar TYPE=${TYPE} --hivevar SOURCE=${SOURCE}"
 
 ${BEELINE}
