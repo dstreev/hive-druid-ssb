a116ed155206534f78ded6a1b200054d2945ef8a
David Streever <david@streever.com>
2018-04-17 12:39:49 -0400

    missing quote

--- 00clean.sh
+++ 00clean.sh
@@ -7,4 +7,4 @@ hdfs dfs -rmr -skipTrash /tmp/ssb/${SCALE}
 
 BEELINE_RAW="beeline -n $HIVE_USER -w $HIVE_PASSWORD_FILE -u "${HS2}"
 
-$BEELINE_RAW -e "drop database if exists ssb_${SCALE}_raw cascade; drop database if exists ssb_${SCALE}_flat_orc cascade;
+$BEELINE_RAW -e "drop database if exists ssb_${SCALE}_raw cascade; drop database if exists ssb_${SCALE}_flat_orc cascade;"
