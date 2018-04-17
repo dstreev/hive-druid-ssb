42060c4b4c10867187070557e8b0107ffbdac8e6
David Streever <david@streever.com>
2018-04-17 12:57:15 -0400

    misspelling

--- 00clean.sh
+++ 00clean.sh
@@ -7,6 +7,6 @@ hdfs dfs -rm -r -skipTrash /tmp/ssb/${SCALE}
 
 BEELINE_RAW="beeline -n ${HIVE_USER} -w ${HIVE_PASSWORD_FILE} -u '${HS2}'"
 
-echo ${BELLINE_RAW}
+echo ${BEELINE_RAW}
 
 ${BEELINE_RAW} -e "drop database if exists ssb_${SCALE}_raw cascade; drop database if exists ssb_${SCALE}_flat_orc cascade;"
