708d046508ec989181bcf9ea8deee8220f396e31
David Streever <david@streever.com>
2018-04-17 14:08:44 -0400

    remove old file

--- 01structor_end_to_end.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#sudo systemctl restart hive2-server2
-#/vagrant/modules/maven/files/install_maven_manually.sh
-#source /vagrant/modules/maven/files/maven.sh
-./00datagen.sh 5 os03.streever.local:10000 os01.streever.local druid hortonworks
-./00load.sh 5 "jdbc:hive2://os02.streever.local:2181,os03.streever.local:2181,os04.streever.local:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2-hive2"
-./00run.sh "jdbc:hive2://os02.streever.local:2181,os03.streever.local:2181,os04.streever.local:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2-hive2"
